a2129747d151e5298455daf1218917a0ee9027aa
Sam Schlinkert <sschlinkert@gmail.com>
2022-01-05 23:35:51 -0500

    bottle working ok

--- bottle.sh
+++ bottle.sh
@@ -12,12 +12,20 @@ then
   mkdir $OUTPUTDIR
   # age --decrypt -i ~/age/archive.txt "$1" | tar -xz -C $OUTPUTDIR
   age --decrypt -i ~/age/archive.txt "$2" | tar -xzP -C $OUTPUTDIR
-fi
-if [ "$1" == "cork" ]
+elif [ "$1" == "cork" ]
 then
-  INPUTDIR="$(dirname "${2}")"
+  if [ -z "$2" ]
+    then
+      echo "No target supplied."
+      exit 1
+  fi
+  # INPUTDIR="$(dirname "${2}")"
   OUTPUTDIR="$(dirname .)"
   OUTPUTDEST="$(basename "${2}")"
   # OUTPUTBASENAME="$(basename "${2}")"
   tar -cz -C "$2" $OUTPUTDIR --absolute-names "$2" | age --encrypt -i ~/age/archive.txt > $OUTPUTDEST.tar.gz.age
+else
+  echo "bottle HELP"
+  echo "Compress and encrypt directories with: bottle cork <path/to/filename>"
+  echo "Extract and decrypt directories with: bottle pop <new_dir_name>"
 fi
