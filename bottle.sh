1c7909873c21ff3391747045305cc344f92f1f3b
Sam Schlinkert <sschlinkert@gmail.com>
2022-01-06 10:30:07 -0500

    better

--- bottle.sh
+++ bottle.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
+KEYFILE=/home/$USER/age/archive.txt
+
+# Check that keyfile exists
+if [ ! -f "$KEYFILE" ]; then
+  echo "$KEYFILE does not exist."
+  echo "Creating an age key-pair for bottle to use by running:"
+  echo "mkdir ~/age && age-keygen -o ~/age/archive.txt"
+  exit 1
+fi
 if [ "$1" == "pop" ]
 then
   if [ -z "$2" ]
     then
-      echo "No target supplied."
-      # return 1
+      echo "No target supplied. Run bottle help for help."
       exit 1
   fi
-  # age --decrypt -i ~/age/archive.txt "$1" | tar -xvz
   OUTPUTDIR="$(basename "${2}" .tar.gz.age)"
   mkdir $OUTPUTDIR
-  # age --decrypt -i ~/age/archive.txt "$1" | tar -xz -C $OUTPUTDIR
-  age --decrypt -i ~/age/archive.txt "$2" | tar -xzP -C $OUTPUTDIR
+  age --decrypt -i $KEYFILE "$2" | tar -xzP -C $OUTPUTDIR
 elif [ "$1" == "cork" ]
 then
   if [ -z "$2" ]
     then
-      echo "No target supplied."
+      echo "No target supplied. Run bottle help for help."
       exit 1
   fi
-  # INPUTDIR="$(dirname "${2}")"
   OUTPUTDIR="$(dirname .)"
   OUTPUTDEST="$(basename "${2}")"
-  # OUTPUTBASENAME="$(basename "${2}")"
-  tar -cz -C "$2" $OUTPUTDIR --absolute-names "$2" | age --encrypt -i ~/age/archive.txt > $OUTPUTDEST.tar.gz.age
+  tar -cz -C "$2" $OUTPUTDIR --absolute-names "$2" | age --encrypt -i $KEYFILE > $OUTPUTDEST.tar.gz.age
 else
   echo "bottle HELP"
   echo ""
