65da0fe4bd56704b04159b336515cfaf258f4565
Sam Schlinkert <sschlinkert@gmail.com>
2022-01-07 13:29:46 -0500

    caps Bottle in install text

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ cp -v bottle.sh $HOME/.local/bin/bottle
 KEYFILE=$HOME/age/archive.txt
 if [ -f "$KEYFILE" ]; then
 	echo "You already have a key at $KEYFILE"
-	echo "bottle will use this key."
+	echo "Bottle will use this key."
 else
 	echo "$KEYFILE does not exist."
-	echo "Creating an age key-pair for bottle to use at $KEYFILE"
+	echo "Creating an age key-pair for Bottle to use at $KEYFILE"
 	mkdir ~/age && age-keygen -o ~/age/archive.txt
 fi
