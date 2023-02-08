d2d010f5359a7eb490436fa070127ac640ea1ad9
Sam Schlinkert <sschlinkert@gmail.com>
2023-02-08 11:41:03 -0500

    adds unofficial strict mode to upgrade script, just in case

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# "Unofficial BASH strcit mode" settings
+# (from http://redsymbol.net/articles/unofficial-bash-strict-mode/)
+set -euo pipefail
+IFS=$'\n\t'
 
 echo "Copying bottle tool to .local/bin/bottle"
 cp -v bottle.sh $HOME/.local/bin/bottle
